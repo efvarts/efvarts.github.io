1e22082189ec01231bf53678a969353cdfa81a78
efvarts <87248384+efvarts@users.noreply.github.com>
2024-09-02 04:58:23 +0000

    Add files via upload

--- Snake/script.js
+++ Snake/script.js
@@ -1,11 +1,5 @@
 var size = prompt("Size of window (px): ");
 
-var newWindow = open('/Snake/snake.html', 'snake', `width=${size},height=${size}`)
+var newWindow = open('https://efvarts.github.io/Snake/snake.html', 'snake', `width=${size},height=${size}`)
+// var newWindow = open('/snake.html', 'snake', `width=${size},height=${size}`)
 newWindow.focus();
-
-// alert(newWindow.location.href); // (*) about:blank, loading hasn't started yet
-
-newWindow.onload = function() {
-//   let html = `<div style="font-size:30px">Welcome!</div>`;
-  newWindow.document.body.insertAdjacentHTML('afterbegin', html);
-};
